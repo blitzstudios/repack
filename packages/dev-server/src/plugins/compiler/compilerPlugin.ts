014b96d2ae234825725207d10da74164af41788e
Luke Herbold <luke@sleeper.app>
2023-06-22 14:29:52 -0700

    Attempt to add content-range support to repack dev server.

--- packages/dev-server/src/plugins/compiler/compilerPlugin.ts
+++ packages/dev-server/src/plugins/compiler/compilerPlugin.ts
@@ -64,17 +64,49 @@ async function compilerPlugin(
           sendProgress
         );
         const mimeType = delegate.compiler.getMimeType(file, platform, asset);
+        const buffer = Buffer.isBuffer(asset) ? asset : Buffer.from(asset);
 
         if (multipart) {
-          const buffer = Buffer.isBuffer(asset) ? asset : Buffer.from(asset);
           multipart.setHeader('Content-Type', `${mimeType}; charset=UTF-8`);
           multipart.setHeader(
             'Content-Length',
             String(Buffer.byteLength(buffer))
           );
-          multipart.end(buffer);
         } else {
-          return reply.code(200).type(mimeType).send(asset);
+          reply.header('Content-Type', `${mimeType}; charset=UTF-8`);
+          reply.header('Content-Length', String(Buffer.byteLength(buffer)));
+        }
+
+        if (request.headers.range) {
+          // Parse the range header to get the start and end bytes
+          const parts = request.headers.range.replace(/bytes=/, '').split('-');
+          const start = parseInt(parts[0], 10);
+          const end = parts[1] ? parseInt(parts[1], 10) : buffer.length - 1;
+
+          // Create a new buffer that only includes the part of the file requested
+          const chunk = buffer.slice(start, end + 1);
+
+          // Set the necessary headers
+          reply.header(
+            'Content-Range',
+            `bytes ${start}-${end}/${buffer.length}`
+          );
+          reply.header('Accept-Ranges', 'bytes');
+
+          if (multipart) {
+            multipart.setHeader('Content-Length', String(chunk.length));
+            multipart.end(chunk);
+          } else {
+            // Send the chunk with a 206 status code
+            return reply.code(206).send(chunk);
+          }
+        } else {
+          // If no range header is present, send the whole file like before
+          if (multipart) {
+            multipart.end(buffer);
+          } else {
+            return reply.code(200).send(asset);
+          }
         }
       } catch (error) {
         request.log.error(error);
