0c69fca4412a055855e12fc1ff12771b619e6435
Luke Herbold <luke@sleeper.app>
2023-08-02 15:08:58 -0700

    Fix issue with `eager` dependencies.

--- packages/repack/src/webpack/plugins/utils/AssetsCopyProcessor.ts
+++ packages/repack/src/webpack/plugins/utils/AssetsCopyProcessor.ts
@@ -45,6 +45,7 @@ export class AssetsCopyProcessor {
 
     // Chunk bundle e.g: `index.bundle`, `src_App_js.chunk.bundle`
     const [chunkFile] = [...chunk.files];
+    if (!chunkFile) return;
     const relatedSourceMap =
       compilation.assetsInfo.get(chunkFile)?.related?.sourceMap;
     // Source map for the chunk e.g: `index.bundle.map`, `src_App_js.chunk.bundle.map`
