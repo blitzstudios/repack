ef3553d07da25ddd5d6c1339adf12cf85137b760
Luke Herbold <luke@sleeper.app>
2024-06-24 17:56:08 -0700

    gitpkg

---
+++ dist/modules/ScriptManager/NativeScriptManager.js
@@ -0,0 +1,6 @@
+import { TurboModuleRegistry } from 'react-native';
+
+// TODO FIX TYPES https://github.com/callstack/repack/issues/494
+
+export default TurboModuleRegistry.get('ScriptManager');
+//# sourceMappingURL=NativeScriptManager.js.map
