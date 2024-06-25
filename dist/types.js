a430b6f4ce7a28975eb8b263dc62723adefe9d9d
Luke Herbold <luke@sleeper.app>
2024-06-24 17:54:27 -0700

    gitpkg

---
+++ dist/types.js
@@ -0,0 +1,45 @@
+export let Server;
+
+/** Representation of the compilation progress. */
+(function (_Server) {
+  /** Development server configuration. */
+  /** Development server options. */
+  /**
+   * A complete delegate with implementations for all server functionalities.
+   */
+  /**
+   * A delegate context used in `delegate` builder in {@link Config}.
+   *
+   * Allows to emit logs, notify about compilation events and broadcast events to connected clients.
+   */
+  /**
+   * Delegate with implementation for logging functions.
+   */
+  /**
+   * Delegate with implementation for messages used in route handlers.
+   */
+  let Api;
+  (function (_Api) {})(Api || (Api = _Server.Api || (_Server.Api = {})));
+})(Server || (Server = {}));
+/**
+ * Type representing a function to send the progress.
+ *
+ * Used by {@link CompilerDelegate} in `getAsset` function to send the compilation
+ * progress to the client who requested the asset.
+ */
+/**
+ * Internal types. Do not use.
+ *
+ * @internal
+ */
+export let Internal;
+(function (_Internal) {
+  let EventTypes;
+  (function (EventTypes) {
+    EventTypes["BuildStart"] = "BuildStart";
+    EventTypes["BuildEnd"] = "BuildEnd";
+    EventTypes["HmrEvent"] = "HmrEvent";
+  })(EventTypes || (EventTypes = {}));
+  _Internal.EventTypes = EventTypes;
+})(Internal || (Internal = {}));
+//# sourceMappingURL=types.js.map
