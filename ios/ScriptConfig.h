1fd2229880e1752fd0e95d5400f6431b99dd8c36
Luke Herbold <luke@sleeper.app>
2024-06-24 17:33:21 -0700

    gitpkg

---
+++ ios/ScriptConfig.h
@@ -0,0 +1,36 @@
+#ifndef ScriptConfig_h
+#define ScriptConfig_h
+
+@interface ScriptConfig : NSObject
+
+NS_ASSUME_NONNULL_BEGIN
+
+@property (readonly) NSString *scriptId;
+@property (readonly) NSURL *url;
+@property (readonly) NSString *method;
+@property (readonly, nullable) NSString *query;
+@property (readonly) BOOL fetch;
+@property (readonly) BOOL absolute;
+@property (readonly, nullable) NSData *body;
+@property (readonly, nullable) NSDictionary *headers;
+@property (readonly) NSNumber *timeout;
+@property (readonly) NSString *verifyScriptSignature;
+
++ (ScriptConfig *)fromConfigDictionary:(NSDictionary *)config withScriptId:(NSString *)scriptId;
+
+- (ScriptConfig *)initWithScript:(NSString *)scriptId
+                         withURL:(NSURL *)url
+                      withMethod:(NSString *)method
+                       withQuery:(nullable NSString *)query
+                       withFetch:(BOOL)fetch
+                    withAbsolute:(BOOL)absolute
+                     withHeaders:(nullable NSDictionary *)headers
+                        withBody:(nullable NSData *)body
+                     withTimeout:(NSNumber *)timeout
+       withVerifyScriptSignature:(NSString *)verifyScriptSignature;
+
+NS_ASSUME_NONNULL_END
+
+@end
+
+#endif /* ScriptConfig_h */
