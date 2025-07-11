ce623c29b33e48c2dfbea24a29a42ca00f2172be
Luke Herbold <luke@sleeper.app>
2025-07-11 09:48:38 -0700

    Fix webpack + downgrade tcp socket.

--- packages/repack/src/commands/webpack/start.ts
+++ packages/repack/src/commands/webpack/start.ts
@@ -79,6 +79,7 @@ export async function start(
   const { start, stop } = await createServer({
     options: {
       ...devServerOptions,
+      host: '0.0.0.0',
       rootDir: cliConfig.root,
       logRequests: showHttpRequests,
     },
